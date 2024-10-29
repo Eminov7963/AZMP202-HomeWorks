f0e956ce76384dae9dd43cfcdca0734584640a4c
Eminov7963 <eminovemin199@gmail.com>
2024-10-30 00:49:45 +0400

    aded

---
+++ 1.1 JS-IF/script.js
@@ -0,0 +1,82 @@
+// let payment = +prompt("emek haqqi");
+// let amount = +prompt("istediyiniz kredit meblegi");
+// let mounth = +prompt("istediyiniz muddet (ay)");
+// let result = 0;
+// if(mounth < 37){
+//     if(amount < payment * 11){
+//         if()
+//     }
+//     else{
+//         console.log("size kredit dusmur")
+//     }
+// }
+// else{
+//     alert("Size kredit dusmur");
+// }
+
+// task1
+// let num = +prompt("ededi qeyd edin")
+// if (num > 80 && num < 100) {
+//   console.log("A");
+// } else if (num > 70 && num < 90) {
+//   console.log("B");
+// } else if (num > 60 && num < 69) {
+//   console.log("C");
+// } else if (num > 50 && num < 59) {
+//   console.log("D");
+// } else if (num > 0 && num < 49) {
+//   console.log("F");
+// }
+
+// task2
+
+// let mounth = prompt("ayi daxil edin")
+// if(mounth === "September" &   mounth === "October" && mounth === "November"){
+//     console.log("it is Autumn")
+// }
+// else if (mounth === "December" && mounth === "January " && mounth === "February") {
+//   console.log("it is Winter");
+// }
+// else if (mounth === "March" && mounth === "April" && mounth === "May") {
+//   console.log("it is Spring");
+// }
+// else if (mounth === "June" && mounth === "July" && mounth === "August") {
+//   console.log("it is Summer");
+// }
+
+// task3
+
+// let num1 = +prompt("1ci ededi daxil edin")
+// let num2 = +prompt("2ci ededi daxil edin");
+// let num3 = +prompt("3ci ededi daxil edin");
+// let sum = num1 + num2 + num3;
+// let AvarageSum = sum / 3;
+// console.log(AvarageSum);
+
+// task4
+// let names = prompt("adi daxil edin");
+// let surname = prompt("soyadi daxil edin");
+// let full_name = names + surname;
+// console.log(full_name)
+
+// 1.1
+// let katet1 = +prompt("1kateti daxil edin");
+// let katet2 = +prompt("2kateti daxil edin");
+// let hipotenuz = +prompt("hipetonu daxil edin")
+// if (katet1 ** 2 + katet2 ** 2 === hipotenuz ** 2)
+//     console.log("duzbucaqli ucbucaqdir")
+// else{
+//     console.log("duzbucaqli-ucbucaq deyil")
+// }
+
+// 2.2
+// let num = +prompt("3 reqemli ededi daxil edin")
+// if(num > 100 && num < 1000){
+//         let hundreds = (num / 100);
+//         let tens = (num / 10) % 10;
+//         let unit = num % 10;
+//         console.log(`Result: ${hundreds}, ${tens}, ${unit}`);
+// }
+// else{
+//     console.log("daxil etediyiniz eded 3 reqemli deyil");
+// }
