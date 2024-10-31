bddbf69c02b750a6bd1105acd3ed954934685efe
Eminov7963 <eminovemin199@gmail.com>
2024-10-31 12:50:44 +0400

    aded

---
+++ 1.JS-variables.control-structure.loops/main.js
@@ -0,0 +1,107 @@
+// Task1
+// let eded = +prompt();
+// let yuzluk = Math.floor(eded / 100);
+// let onluq = Math.floor((eded % 100) / 10);
+// let teklik = eded % 10;
+// let sum = yuzluk + onluq +teklik;
+// let hasil = yuzluk * onluq * teklik;
+// let ededi_orta = sum / String(eded).length;
+// console.log(ededi_orta);
+
+// Task2
+// let n = +prompt("daxil edin")
+// for (let i = 1; i < n; i++){
+//     if(n % i == 0){
+        
+//         console.log(i); 
+//     }
+    
+// }
+
+// Task3
+// let n = +prompt("daxil edin")
+// let say = 0;
+// for (let i = 1; i < n; i++){
+//     if(n % i == 0){
+//         say++
+           
+        
+//     }
+    
+// }
+// console.log(say); 
+
+// Task4
+// let kvadrat;
+// for (let i = 0; i <= 10; i++) {
+//     kvadrat= i*i
+//     console.log(`${i} x ${i} = ${kvadrat}`);
+     
+    
+// }
+// Task5
+// let kub;
+// for (let i = 0; i <= 10; i++) {
+//   kub = i * i * i;
+//   console.log(`${i} x ${i} x ${i} = ${kub}`);
+// }
+
+// Task6;
+// let cütCəm = 0;
+// let təkCəm = 0;
+// let sum = 0;
+// for (let i = 1; i < 100; i++) {
+//     if (i % 2 === 0) {
+      
+//       cütCəm += i; 
+//     } else {
+      
+//       təkCəm += i; 
+//     } 
+    
+// }
+// sum = cütCəm + təkCəm;
+// console.log(sum);
+
+// Task7
+// let arr = [4, 2, 7, 9, 3, 12, 46, 32, 19];
+// let big = arr[0];
+// let small = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//     if(big < arr[i]){
+//         big = arr[i];
+//     }
+//     else if(small > arr[i]){
+//         small = arr[i];
+//     }
+// }
+// console.log(big);
+// console.log(small);
+
+// Task8
+// let arr = [4, 2, 7, 9, 3, 12, 46, 32, 19];
+// let big = arr[0];
+// let small = arr[0];
+// let ededi_orta = 0
+// for (let i = 0; i < arr.length; i++) {
+//   if (big < arr[i]) {
+//     big = arr[i];
+//   } else if (small > arr[i]) {
+//     small = arr[i];
+//   }
+// }
+// ededi_orta = (big + small) / 2;
+// console.log(ededi_orta);
+
+// Task9
+// let sampleNews = `As software developers, the natural tendency is to start developing applications based on your ownhands-on experience and knowledge right away. However, overtime issues in the application arise, adaptations to changes,
+// and new features happen.`;
+// let bosluq = sampleNews.split(" ").length -1 ;
+// console.log(bosluq);
+
+// Exstra
+// let eded;
+// for (let i = 0; i < 100; i++) {
+//     if(i > 3 && i % )
+    
+// }
