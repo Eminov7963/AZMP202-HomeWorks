610712e6492e668c3f7bc26910a1460a5091ea20
Eminov7963 <eminovemin199@gmail.com>
2024-11-20 19:11:01 +0400

    first commit push

---
+++ 1.3 JS- basket, login register/js/helper.js
@@ -0,0 +1,6 @@
+export function setDatalocalstorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+export function getDatalocalstorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
