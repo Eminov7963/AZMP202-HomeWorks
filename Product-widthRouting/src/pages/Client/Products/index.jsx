a5b03d74972d2f339ce29b8d4e4bad3d21fd3478
feridshahmur <faridfs-102@code.edu.az>
2024-12-27 19:21:29 +0400

    aded

--- Product-widthRouting/src/pages/Client/Products/index.jsx
+++ Product-widthRouting/src/pages/Client/Products/index.jsx
@@ -1,27 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useEffect, useState } from 'react'
 import controller from '../../../services/helper'
 import { endpoints } from '../../../services/costants'
+import { Link } from "react-router-dom"
+import { Card, Col, Row } from 'antd';
+const { Meta } = Card;
 
 const Products = () => {
   const [products, setData] = useState([])
   const Getproducts = async () => {
     try {
       const data = await controller.getAllData(endpoints.products);
+      console.log(data);
       setData(data);
     } catch (error) {
       console.log(error);
-      
-    }  
+
+    }
   };
-  useEffect(()=>{
-    Getproducts;
-  },[])
+
+
+  useEffect(() => {
+    Getproducts();
+  }, [])
   return (
-    <div>
-      {products.length > 0 &&
-        products.map((q) => <h1 key={q.id}>{q.title}</h1>)}
-    </div>
+    <Row gutter={16}>
+      {products && products.map((p) => {
+        return (<Col className="gutter-row" span={6} key={p.id}>
+          <Card
+            hoverable
+            style={{ width: 240 }}
+            cover={<img alt={p.title} src={p.image} />}
+          >
+
+            <Meta title={<Link to={`${p.id}`}>{p.title}</Link>} description={<div>
+
+              <p>Price: $ {p.price}</p>
+              <p>{p.description.slice(0, 50)}</p>
+              
+            </div>} />
+          </Card>
+        </Col>)
+      })}
+    </Row>
   );
 }
 
 export default Products
+
