0e3710ef85a09dbccedb06780cc13c76815b4a68
ilgarguliyevv <s.aliyev1214@gmail.com>
2025-01-14 14:16:00 +0400

    aded

--- full-stack-practise/client/src/pages/Client/Wishlist/index.jsx
+++ full-stack-practise/client/src/pages/Client/Wishlist/index.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { FaHeart } from "react-icons/fa";
-import {toggleFavorites} from "../../../redux/features/wishlistSlice"
+import { toggleFavorites } from "../../../redux/features/wishlistSlice";
+import styles from './Wishlist.module.scss';
 
 const Wishlist = () => {
   const wishlist = useSelector((state) => state.wishlist);
@@ -9,15 +10,18 @@ const Wishlist = () => {
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <div className={styles.wishlistContainer}>
       {wishlist.items.length === 0 ? (
-        <h2>Wihslist is empty!</h2>
+        <h2 className={styles.emptyMessage}>Wishlist is empty!</h2>
       ) : (
         wishlist.items.map((q) => {
           return (
-            <div key={q.id}>
-              <span>{q.title}</span>
+            <div key={q._id} className={styles.wishlistItem}>
+              <span className={styles.itemTitle}>{q.title}</span>
+              <h6 className={styles.itemDescription}>{q.description}</h6>
+              <span className={styles.itemPrice}>{q.price}</span>
               <button
+                className={styles.heartButton}
                 onClick={() => {
                   dispatch(toggleFavorites(q));
                 }}
