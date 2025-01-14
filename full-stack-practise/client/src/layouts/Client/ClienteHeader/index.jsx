d3c7f5fd209d7bafb80b5b8ec8984d9154ae8bc5
ilgarguliyevv <s.aliyev1214@gmail.com>
2025-01-14 19:33:38 +0400

    aded

--- full-stack-practise/client/src/layouts/Client/ClienteHeader/index.jsx
+++ full-stack-practise/client/src/layouts/Client/ClienteHeader/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
-import "./client.css"
+import styles from "./client.module.scss"
 import { useSelector } from "react-redux";
+import { IoCall } from "react-icons/io5";
 const ClientHeader = () => {
     const wishlist = useSelector((state) => state.wishlist);
   return (
     <header>
+      <section className={styles.head}> 
+        <h1>We believe we helps people
+        for happier lives</h1>
+        <img src="https://preview.colorlib.com/theme/immigration/img/logo.png" alt="" />
+        <div className={styles.call}>
+        <span>+880 123 12 658 439 </span>
+        <div className={styles.callicon}><IoCall /></div>
+        </div>
+      </section>
       <nav>
         <ul>
           <li>
--- full-stack-practise/client/src/pages/Client/Home/index.jsx
+++ full-stack-practise/client/src/pages/Client/Home/index.jsx
@@ -9,6 +9,7 @@ import {
 } from "../../../redux/services/products";
 import styles from "./index.module.scss";
 
+
 const Home = () => {
   const {
     data: productsData,
@@ -46,44 +47,93 @@ const Home = () => {
   };
 
   return (
-    <div className={styles.productsContainer}>
-      {products.map((product) => (
-        <div key={product._id} className={styles.productCard}>
-          
-         
-          <h3>{product.title}</h3>
-          <p>{product.description}</p>
-          <p className={styles.price}>Price: ${product.price}</p>
-          <div className={styles.buttonContainer}>
-            <button
-              className={styles.deleteButton}
-              onClick={() => handleDelete(product._id)}
-            >
-              Delete
-            </button>
-
-            <button
-              className={styles.detailsButton}
-              onClick={() => handleDetails(product._id)}
-            >
-              Details
-            </button>
-
-            <button
-              onClick={() => {
-                dispatch(toggleFavorites(product));
-              }}
-            >
-              {!wishlist?.items.find((q) => q.id === product._id) ? (
-                <FaRegHeart />
-              ) : (
-                <FaHeart />
-              )}
-            </button>
+    <>
+      <section className={styles.mainsection}>
+        <div className={styles.container}>
+          <div className={styles.left}>
+            <h6>s Visa without within hoursIm</h6>
+            <h1>Immigrations & Visa Consultation</h1>
+            <button className={styles.consultancy}>Book Consultancy</button>
+          </div>
+          <div className={styles.right}>
+            <img src="https://preview.colorlib.com/theme/immigration/img/header-img.png" alt="" />
           </div>
         </div>
-      ))}
-    </div>
+      </section>
+
+      <section className={styles.products}>
+      <div className={styles.productsContainer}>
+        {products.map((product) => (
+          <div key={product._id} className={styles.productCard}>
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            <p className={styles.price}>Price: ${product.price}</p>
+            <div className={styles.buttonContainer}>
+              <button
+                className={styles.deleteButton}
+                onClick={() => handleDelete(product._id)}
+              >
+                Delete
+              </button>
+
+              <button
+                className={styles.detailsButton}
+                onClick={() => handleDetails(product._id)}
+              >
+                Details
+              </button>
+
+              <button
+                onClick={() => {
+                  dispatch(toggleFavorites(product));
+                }}
+              >
+                {!wishlist?.items.find((q) => q.id === product._id) ? (
+                  <FaRegHeart />
+                ) : (
+                  <FaHeart />
+                )}
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      </section>
+      <section className={styles.cards}>
+        <div className={styles.top}>
+          <h1>Requirements to be Immigrants</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.boxs}>
+            <img src="https://preview.colorlib.com/theme/immigration/img/s1.jpg" alt="" />
+            <div>
+            <div className={styles.country}>United States</div>
+            <h3>Addiction When Gambling Becomes A Problem</h3>
+            <p>inappropriate behavior ipsum dolor sit amet, consectetur.</p>
+            </div>
+          </div>
+          <div className={styles.boxs}>
+            <img src="https://preview.colorlib.com/theme/immigration/img/s2.jpg" alt="" />
+            <div className={styles.country}>United States</div>
+            <h3>Addiction When Gambling Becomes A Problem</h3>
+            <p>inappropriate behavior ipsum dolor sit amet, consectetur.</p>
+          </div>
+          <div className={styles.boxs}>
+            <img src="https://preview.colorlib.com/theme/immigration/img/s3.jpg" alt="" />
+            <div className={styles.country}>United States</div>
+            <h3>Addiction When Gambling Becomes A Problem</h3>
+            <p>inappropriate behavior ipsum dolor sit amet, consectetur.</p>
+          </div>
+          <div className={styles.boxs}>
+            <img src="https://preview.colorlib.com/theme/immigration/img/s4.jpg" alt="" />
+            <div className={styles.country}>United States</div>
+            <h3>Addiction When Gambling Becomes A Problem</h3>
+            <p>inappropriate behavior ipsum dolor sit amet, consectetur.</p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
